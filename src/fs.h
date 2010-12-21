2305bec8aedecd1a56e68d791a4a62475664396f
n4ru <geokoder@gmail.com>
2010-12-21 13:22:34 -0500

    mkDir -> fsMkdir. Now return true if the directory was created successfully, returns false if the directory already exists or creation was unsuccessful. fsRename added. Returns true if it renamed successfully, returns false if the directory doesn't exist or the new directory already exists.

--- src/fs.h
+++ src/fs.h
@@ -7,13 +7,27 @@
 
 #include <cell/cell_fs.h> 
 
-void mkdir(char* newDir) {
-	cellFsMkdir(newDir, CELL_FS_DEFAULT_CREATE_MODE_1);
-}
-
 char fsStat(char* filename) {
 	CellFsStat sb;
 	return cellFsStat(filename, &sb) == CELL_FS_SUCCEEDED;
+}
+
+bool fsMkdir(char* newDir) {
+	if(fsStat(newDir)) {
+		return false;
+	} else {
+		cellFsMkdir(newDir, CELL_FS_DEFAULT_CREATE_MODE_1);
+		return fsStat(newDir);
+	}
+}
+
+bool fsRename(char* from, char* to) {
+	if(!fsStat(to) && fsStat(from)) {
+		cellFsRename(from, to);
+		return fsStat(to);
+	} else {
+		return 1;
+	}
 }
 
 #endif /* __FS_H */
